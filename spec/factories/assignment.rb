4eb0d20af0961ed7998d4452549cff300f7a3f6d
Steven Clontz <steven.clontz@gmail.com>
2015-03-26 10:38:38 -0500

    cleaner associations

--- spec/factories/assignment.rb
+++ spec/factories/assignment.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :assignment do
     name "Group 20XX"
     description "assignment of these notes to my small group"
-    workbook { create(:workbook) }
+    workbook
   end
 end
